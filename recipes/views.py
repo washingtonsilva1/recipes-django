cde669c1200e087bc5f1d2b5ca3d1268872f5aab
Deivison Washington <deivisonwashington@hotmail.com>
2023-08-15 16:58:59 -0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(req):
-    return render(req, 'recipes/home.html',
-                  context={'name': 'Deivison'}, status=200)
-
-
-def contact(req):
-    return HttpResponse('Contato!')
-
-
-def about(req):
-    return HttpResponse('Sobre!')
+    return render(req, 'recipes/home.html')
