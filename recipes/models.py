5d518f6177322f52dd9ca3ea1ba4519d6e30c576
Deivison Washington <deivisonwashington@hotmail.com>
2023-08-23 13:50:58 -0300

    Adicionando Recipes na admin do Django

--- recipes/models.py
+++ recipes/models.py
@@ -27,3 +27,6 @@ class Recipe(models.Model):
     category = models.ForeignKey(Category,
                                  on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 app_name = 'recipes'
@@ -8,3 +10,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
