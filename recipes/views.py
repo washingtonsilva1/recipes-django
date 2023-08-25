ad59550515e67b06f6d69e5ce51a717a0c538141
Deivison Washington <deivisonwashington@hotmail.com>
2023-08-24 23:28:12 -0300

    Filtrando is_published

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
-from utils.recipe.factory import make_recipe
+# from utils.recipe.factory import make_recipe
 from .models import Recipe
 
 
 def home(req):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(req, 'recipes/pages/home.html',
                   {'recipes': recipes})
 
 
 def category(req, id):
-    recipes = Recipe.objects.filter(category__id=id).order_by('-id')
-    return render(req, 'recipes/pages/home.html',
+    recipes = Recipe.objects.filter(
+        category__id=id, is_published=True).order_by('-id')
+    return render(req, 'recipes/pages/categoryView.html',
                   {'recipes': recipes})
 
 
 def recipe(req, id):
+    recipe = Recipe.objects.get(pk=id)
     return render(req, 'recipes/pages/recipeView.html',
-                  {'recipe': make_recipe(), 'is_detail_view': True})
+                  {'recipe': recipe, 'is_detail_view': True})
