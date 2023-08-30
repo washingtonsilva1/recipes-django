6520f39156d9b43b3e1929d12714f75de020295a
Deivison Washington <deivisonwashington@hotmail.com>
2023-08-30 16:13:52 -0300

    🧪 test: Separando testes e tests.py deletado

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,20 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from . import views
-
-
-class RecipeURLsTest(TestCase):
-    def test_recipes_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipes_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs=({'id': 1}))
-        self.assertEqual(url, '/category/1/')
-
-    def test_recipes_recipe_url_is_correct(self):
-        url = reverse('recipes:recipe', args=[1])
-        self.assertEqual(url, '/recipes/1/')
+from recipes import views
 
 
 class RecipeViewsTest(TestCase):
