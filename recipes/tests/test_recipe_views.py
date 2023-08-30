79863f934c599f0e1ce5466759b2ad0fb8cf19bd
Deivison Washington <deivisonwashington@hotmail.com>
2023-08-30 17:37:42 -0300

    🧪 test: Testes category e recipe retornam 404

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -8,15 +8,7 @@ class RecipeViewsTest(TestCase):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
-    def test_recipes_category_view_is_correct(self):
-        view = resolve(reverse('recipes:category', args=[1]))
-        self.assertIs(view.func, views.category)
-
-    def test_recipes_recipe_view_is_correct(self):
-        view = resolve(reverse('recipes:recipe', args=[1]))
-        self.assertIs(view.func, views.recipe)
-
-    def test_recipes_home_view_status_code_200_ok(self):
+    def test_recipes_home_view_returns_status_code_200_ok(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEquals(response.status_code, 200)
 
@@ -24,6 +16,22 @@ class RecipeViewsTest(TestCase):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
-    def test_recipes_home_view_recipes_not_found(self):
+    def test_recipes_home_returns_not_found_if_there_are_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn('There are no recipes', response.content.decode('utf-8'))
+
+    def test_recipes_category_view_is_correct(self):
+        view = resolve(reverse('recipes:category', args=[1]))
+        self.assertIs(view.func, views.category)
+
+    def test_recipes_category_returns_404_if_not_exists(self):
+        response = self.client.get(reverse('recipes:category', args=[1]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipes_recipe_view_is_correct(self):
+        view = resolve(reverse('recipes:recipe', args=[1]))
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipes_recipe_returns_404_if_not_exists(self):
+        response = self.client.get(reverse('recipes:recipe', args=[1]))
+        self.assertEqual(response.status_code, 404)
