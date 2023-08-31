c568f385ba4cbbe969844e42272fd908d5df44f7
Deivison Washington <deivisonwashington@hotmail.com>
2023-08-31 18:23:26 -0300

    :test_tube: test: Testando se recipe aparece no template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -19,6 +20,32 @@ class RecipeViewsTest(TestCase):
     def test_recipes_home_returns_not_found_if_there_are_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn('There are no recipes', response.content.decode('utf-8'))
+
+    def test_recipes_home_template_loads_recipes(self):
+        category = Category.objects.create(name='First category')
+        user = User.objects.create_user(
+            first_name='first',
+            last_name='last',
+            username='deivison',
+            email='deivisonwashington@gmail.com',
+            password='DeivisonWashington'
+        )
+        recipe = Recipe.objects.create(
+            title='Testing recipes',
+            description='Recipe description',
+            slug='testing-recipes',
+            preparation_time=5,
+            preparation_time_unit='Hours',
+            servings=1,
+            servings_unit='Pessoa',
+            preparation_steps='Recipe\'s steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+            category=category,
+            user=user,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.context['recipes'].first(), recipe)
 
     def test_recipes_category_view_is_correct(self):
         view = resolve(reverse('recipes:category', args=[1]))
