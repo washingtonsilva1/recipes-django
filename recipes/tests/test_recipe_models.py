472b70ce7041cb71ab9487f7c1b8ce9f3f7b3994
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-09-04 17:52:45 -0300

    :test_tube: test: Teste recipe title maior que 65 caracteres

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -1,7 +1,13 @@
 from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
 
 
 class TestRecipeModel(RecipeTestBase):
     def setUp(self):
         self.recipe = self.make_recipe()
         super().setUp()
+
+    def test_recipe_title_raises_error_if_larger_than_65_chars(self):
+        self.recipe.title = 'test' * 17
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
