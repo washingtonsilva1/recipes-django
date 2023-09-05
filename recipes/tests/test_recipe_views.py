1be6b346a01cdc65d544e7dc115028f6433436dd
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-09-04 22:09:37 -0300

    :recycle: refactor: Alterado assertEquals para assertEqual

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -11,7 +11,7 @@ class RecipeViewsTest(RecipeTestBase):
 
     def test_recipes_home_view_returns_status_code_200_ok(self):
         response = self.client.get(reverse('recipes:home'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_recipes_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
