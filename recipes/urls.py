a789a51ff78629a30e3c29fce61abbe097a987ad
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-09-08 12:45:00 -0300

    test: Nova url search para recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<int:id>/', views.category, name='category'),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
+    path('recipes/search/', lambda request: ..., name='search'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
