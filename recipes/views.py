62219d45db3e51019fbe871c48b7e2b064d2eb40
Deivison Washington <deivisonwashington@hotmail.com>
2023-09-11 22:05:34 -0300

    feat: Campo de busca filtra por título e descrição

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import Http404
 from .models import Recipe
+from django.db.models import Q
 
 
 def home(req):
@@ -18,7 +19,7 @@ def category(req, id):
                    'title': f'{recipes[0].category.name} - Category | '})
 
 
-def recipe(req, id):
+def detail(req, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(req, 'recipes/pages/recipeView.html',
                   {'recipe': recipe, 'is_detail_view': True,
@@ -29,6 +30,11 @@ def search(req):
     search_term = req.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
     return render(req, 'recipes/pages/search.html', context={
         'search_term': search_term,
+        'recipes': recipes,
     })
