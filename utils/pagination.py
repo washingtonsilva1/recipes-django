6ba6acf1089afc7d928fcd2bf29731604e085b15
Deivison Washington <deivisonwashington@hotmail.com>
2023-09-18 22:01:21 -0300

    refactor: Deixando make_pagination_range dinâmico

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,23 +2,23 @@ from math import ceil
 
 
 def make_pagination_range(page_range, pages, current):
-    middle_page = ceil(pages/2)
+    middle_page = ceil(pages / 2)
     start_page = current - middle_page
     last_page = current + middle_page
     total = len(page_range)
-    if current >= total-2:
+    if current >= total - middle_page:
         last_page = total
-        start_page = total-4
+        start_page = total - pages
     if current < middle_page:
         start_page = 0
-        last_page = 4
+        last_page = pages
     return {
         'pagination': page_range[start_page:last_page],
         'page_range': page_range,
         'start_page': start_page,
         'qty_pages': pages,
-        'last_page': last_page,
+        'last_page': total,
         'current': current,
         'first_page_out_of_page_range': current > middle_page,
-        'last_page_out_of_page_range': current < total,
+        'last_page_out_of_page_range': current < total - middle_page,
     }
