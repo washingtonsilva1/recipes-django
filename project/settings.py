4096007b096cadf1a828e7a28ad02548676d28f5
Deivison Washington <deivisonwashington@hotmail.com>
2023-09-20 22:51:50 -0300

    build: Nova dependência em sweetify

--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes.apps.RecipesConfig',
+    'sweetify',
 ]
 
 MIDDLEWARE = [
@@ -132,6 +133,10 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Sweetify settings
+
+SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
