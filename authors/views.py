4ece156e2f3c9157da1a4caaf770993989d0577c
Deivison Washington <deivisonwashington@hotmail.com>
2023-09-25 21:45:38 -0300

    refactor: Melhorando o formulário de authors

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 
 # Create your views here.
 def register(req):
-    return render(req, 'authors/pages/registerView.html')
+    if req.POST:
+        form = RegisterForm(req.POST)
+    else:
+        form = RegisterForm()
+    return render(req, 'authors/pages/registerView.html', {'form': form})
