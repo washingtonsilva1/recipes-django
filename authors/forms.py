86b620f250b436960e1d90aeca69855c6ddd0f67
Deivison Washington <deivisonwashington@hotmail.com>
2023-09-25 21:45:14 -0300

    ci: Criando formulários

---
+++ authors/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'email',
+        ]
