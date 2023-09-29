6d087b786a6047a039e5e1e7f84b4aa93556558e
Deivison Washington <deivisonwashington@hotmail.com>
2023-09-28 21:12:39 -0300

    feat: checar a força da senha

--- authors/forms.py
+++ authors/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
@@ -11,6 +12,15 @@ def set_placeholder(field, placeholder):
     set_attribute(field, 'placeholder', placeholder)
 
 
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    if not regex.match('password'):
+        raise ValidationError(
+            'Password must have at least one uppercase and '
+            'lowercase letter and one number. The length should be '
+            'at least 8 characters.', code='invalid')
+
+
 class RegisterForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
@@ -21,6 +31,7 @@ class RegisterForm(forms.ModelForm):
             field=self.fields['first_name'], placeholder='Ex.: Jonathan')
         set_placeholder(
             field=self.fields['last_name'], placeholder='Ex.: Joestar')
+        self.fields['password'].validators.append(strong_password)
 
     password2 = forms.CharField(
         label='Confirm password',
