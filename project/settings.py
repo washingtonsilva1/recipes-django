b3b0b01a68e1eb7db2631dd481e4aeffc39ce105
Deivison Washington <deivisonwashington@hotmail.com>
2023-10-01 14:28:59 -0300

    build: removendo linha de configuração de sweetify

--- project/settings.py
+++ project/settings.py
@@ -135,10 +135,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Sweetify settings
-
-SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
