d668846af530302499924b192eb6d7a5100a892a
Deivison Washington <deivisonwashington@hotmail.com>
2023-10-09 16:48:03 -0300

    test: testando view create retorna 404 se não há post

--- authors/tests/test_register_view.py
+++ authors/tests/test_register_view.py
@@ -11,3 +11,7 @@ class RegisterViewTest(TestCase):
     def test_register_view_is_rendering_correct_template(self):
         response = self.client.get(reverse('authors:register'))
         self.assertTemplateUsed(response, 'authors/pages/registerView.html')
+
+    def test_register_view_raises_404_if_there_is_no_post_request(self):
+        response = self.client.get(reverse('authors:create'))
+        self.assertEqual(response.status_code, 404)
