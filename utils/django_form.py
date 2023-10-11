afb7e39387e9234a54cad2954ff03425773342b2
Deivison Washington <deivisonwashington@hotmail.com>
2023-10-10 23:55:37 -0300

    style: agrupando funções auxiliares usadas em forms

---
+++ utils/django_form.py
@@ -0,0 +1,10 @@
+import re
+from django.core.exceptions import ValidationError
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    if not regex.match(password):
+        raise ValidationError(
+            'You password doesn\'t match the requirements.', code='invalid')
+    return True
