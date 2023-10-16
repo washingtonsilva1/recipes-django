15d2eb00f10ca1e8d88e3b495163c492e0c42501
Deivison Washington <deivisonwashington@hotmail.com>
2023-10-16 15:56:01 -0300

    test: testes para a view logout

---
+++ authors/tests/test_logout_view.py
@@ -0,0 +1,29 @@
+from authors import views
+from .test_register_base import FormTestBase
+from django.urls import resolve, reverse
+
+
+class LogoutViewTest(FormTestBase):
+    def test_logout_is_loading_correct_view(self):
+        view = resolve(reverse('authors:logout'))
+        self.assertIs(view.func, views.logout_view)
+
+    def test_logout_view_redirect_to_login_when_not_logged(self):
+        response = self.client.get(
+            reverse('authors:logout'),
+            follow=True)
+        self.assertEqual(reverse('authors:login'),
+                         response.request.get('PATH_INFO'))
+
+    def test_logout_view_is_logging_out_user(self):
+        self.create_user()
+        self.client.post(
+            reverse('authors:login_create'),
+            data={'username': self.form_data['username'],
+                  'password': self.form_data['password']})
+        response = self.client.post(reverse('authors:logout'),
+                                    data={
+            'username': self.form_data['username'],
+        }, follow=True)
+        content = response.content.decode('utf-8')
+        self.assertIn('You have sucessfully logged out', content)
