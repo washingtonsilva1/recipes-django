ee6f13c695e4d7e17ba608d7093f7d8bf517d908
Deivison Washington <deivisonwashington@hotmail.com>
2023-10-16 15:54:05 -0300

    test: alterando referência à view login

--- authors/tests/test_login_view.py
+++ authors/tests/test_login_view.py
@@ -6,7 +6,7 @@ from authors import views
 class LoginViewTest(TestCase):
     def test_login_is_loading_correct_view(self):
         view = resolve(reverse('authors:login'))
-        self.assertIs(view.func, views.login)
+        self.assertIs(view.func, views.login_view)
 
     def test_login_view_is_rendering_correct_template(self):
         response = self.client.get(reverse('authors:login'))
