45e416bab4137130465a71c8ecc78c90a31b9800
Deivison Washington <deivisonwashington@hotmail.com>
2023-10-16 15:51:41 -0300

    feat: usuário autenticado pode fazer logout

--- authors/urls.py
+++ authors/urls.py
@@ -5,8 +5,9 @@ from django.urls import path
 app_name = 'authors'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
     path('login/create/', views.login_create, name='login_create'),
     path('register/create/', views.register_create, name='register_create'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -3,10 +3,12 @@ from django.http import Http404
 from django.contrib import messages
 from .forms import RegisterForm, LoginForm
 from django.urls import reverse
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-def register(req):
+def register_view(req):
     register_form_data = req.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
     return render(req, 'authors/pages/registerView.html',
@@ -29,11 +31,13 @@ def register_create(req):
         user.save()
         messages.success(req, "Your user has been created, please log in!")
         del (req.session['register_form_data'])
+        return redirect('authors:login')
     return redirect('authors:register')
 
 
-def login(req):
-    form = LoginForm()
+def login_view(req):
+    login_form_data = req.session.get('login_form_data', None)
+    form = LoginForm(login_form_data)
     return render(req, 'authors/pages/loginView.html', {
         'form': form,
         'form_action': reverse('authors:login_create'),
@@ -41,4 +45,32 @@ def login(req):
 
 
 def login_create(req):
-    ...
+    if not req.POST:
+        raise Http404()
+
+    POST = req.POST
+    req.session['login_form_data'] = POST
+    form = LoginForm(POST)
+    redirect_to = 'authors:login'
+    if form.is_valid():
+        user_auth = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+        if user_auth is not None:
+            messages.success(req, 'You have logged in!')
+            login(req, user_auth)
+            del (req.session['login_form_data'])
+            redirect_to = 'recipes:home'
+        else:
+            messages.error(req, 'Incorrect username or password')
+    return redirect(redirect_to)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(req):
+    if not req.POST or req.POST and req.POST.get('username') != req.user.username:
+        return redirect('authors:login')
+    logout(req)
+    messages.info(req, 'You have sucessfully logged out')
+    return redirect('authors:login')
