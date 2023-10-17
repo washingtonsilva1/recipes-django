71219971ff0657f283a75a4d82bcb7567ec5f881
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-10-17 00:09:57 -0300

    test: adicionando marcador aos testes funcionais

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -1,7 +1,9 @@
+import pytest
 from tests.functional_tests.function_test_base import FunctionalTestBase
 from selenium.webdriver.common.by import By
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(FunctionalTestBase):
     def test_recipe_home_page_when_there_are_no_recipes(self):
         self.chrome.get(self.live_server_url)
