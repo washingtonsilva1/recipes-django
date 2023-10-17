c42c258bfdf39a6ac72d89498800a70bed8ae7c0
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-10-16 22:23:14 -0300

    fix: valor padrão caso não exista a variável desejada

--- utils/browser.py
+++ utils/browser.py
@@ -5,7 +5,7 @@ from time import sleep
 
 def make_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
-    if os.environ.get('CHROMEDRIVER_OPTIONS').strip():
+    if os.environ.get('CHROMEDRIVER_OPTIONS', '').strip():
         for option in os.environ.get('CHROMEDRIVER_OPTIONS').split(','):
             chrome_options.add_argument(option.strip())
     return webdriver.Chrome(options=chrome_options)
