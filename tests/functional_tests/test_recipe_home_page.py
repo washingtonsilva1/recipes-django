0a3293824326a58fee38a28c01688112bfb99960
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-10-16 22:14:40 -0300

    test: refatorando teste

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -1,18 +1,9 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from utils.browser import make_chrome_driver
+from tests.functional_tests.function_test_base import FunctionalTestBase
 from selenium.webdriver.common.by import By
 
 
-class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.chrome = make_chrome_driver('--headless')
-        super().setUp()
-
-    def tearDown(self):
-        self.chrome.quit()
-        super().tearDown()
-
-    def test_home_page_when_there_are_no_recipes(self):
+class RecipeHomePageFunctionalTest(FunctionalTestBase):
+    def test_recipe_home_page_when_there_are_no_recipes(self):
         self.chrome.get(self.live_server_url)
         body = self.chrome.find_element(By.TAG_NAME, 'body')
         self.assertIn('There are no recipes...', body.text)
