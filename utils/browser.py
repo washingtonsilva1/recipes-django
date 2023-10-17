3a846a109f5c3e8a5cb08e249a0fad9d5f74caf9
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-10-16 22:13:39 -0300

    refactor: obtendo opções via dotenv

--- utils/browser.py
+++ utils/browser.py
@@ -1,17 +1,18 @@
+import os
 from selenium import webdriver
 from time import sleep
 
 
-def make_chrome_driver(*options):
+def make_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
-    if options is not None:
-        for option in options:
-            chrome_options.add_argument(option)
+    if os.environ.get('CHROMEDRIVER_OPTIONS').strip():
+        for option in os.environ.get('CHROMEDRIVER_OPTIONS').split(','):
+            chrome_options.add_argument(option.strip())
     return webdriver.Chrome(options=chrome_options)
 
 
 if __name__ == '__main__':
-    browser = make_chrome_driver('--headless')
+    browser = make_chrome_driver()
     browser.get('https://www.google.com.br')
     sleep(5)
     browser.quit()
