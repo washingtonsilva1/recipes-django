dba85323ddbf7eb0b4d8580c79e1dbc5098448a9
Deivison Washington <deivisonwashington@hotmail.com>
2023-10-20 01:08:28 -0300

    test: testando a busca por recipe específico

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -1,11 +1,37 @@
 import pytest
-from tests.functional_tests.function_test_base import FunctionalTestBase
+from unittest.mock import patch
+from tests.functional_tests.functional_test_base import FunctionalTestBase
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(FunctionalTestBase):
+    @patch('recipes.views.RECIPES_PER_PAGE', new=3)
     def test_recipe_home_page_when_there_are_no_recipes(self):
         self.chrome.get(self.live_server_url)
         body = self.chrome.find_element(By.TAG_NAME, 'body')
         self.assertIn('There are no recipes...', body.text)
+
+    @patch('recipes.views.RECIPES_PER_PAGE', new=3)
+    def test_recipe_home_search_input_get_correct_recipe(self):
+        title_needed = 'That is what I need'
+        recipes = self.make_recipe_sample()
+        recipes[0].title = title_needed
+        recipes[0].save()
+        self.chrome.get(self.live_server_url)
+        # Steps:
+        # 1. Get to the page OK
+        # 2. Get to the search input OK
+        # 3. Search a recipe OK
+        # 4. Look if the searched recipe was found OK
+        input = self.chrome.find_element(
+            By.XPATH,
+            "//input[@placeholder='Search a recipe']"
+        )
+        input.send_keys(recipes[0].title)
+        input.send_keys(Keys.ENTER)
+        self.assertIn(
+            title_needed,
+            self.get_chrome_element(By.CLASS_NAME, 'main-content-list')
+        )
