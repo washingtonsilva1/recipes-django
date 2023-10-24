28099cec47cbb0e4e4f9090568dfd65b74c6e66d
Deivison Washington <deivisonwashington@hotmail.com>
2023-10-24 11:33:27 -0300

    test: testando paginação usando selenium

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -19,12 +19,12 @@ class RecipeHomePageFunctionalTest(FunctionalTestBase):
         recipes = self.make_recipe_sample()
         recipes[0].title = title_needed
         recipes[0].save()
-        self.chrome.get(self.live_server_url)
         # Steps:
         # 1. Get to the page OK
         # 2. Get to the search input OK
         # 3. Search a recipe OK
         # 4. Look if the searched recipe was found OK
+        self.chrome.get(self.live_server_url)
         input = self.chrome.find_element(
             By.XPATH,
             "//input[@placeholder='Search a recipe']"
@@ -33,5 +33,16 @@ class RecipeHomePageFunctionalTest(FunctionalTestBase):
         input.send_keys(Keys.ENTER)
         self.assertIn(
             title_needed,
-            self.get_chrome_element(By.CLASS_NAME, 'main-content-list')
-        )
+            self.get_chrome_element(By.CLASS_NAME,
+                                    'main-content-list').text)
+
+    @patch('recipes.views.RECIPES_PER_PAGE', new=3)
+    def test_recipe_home_pagination_more_than_one_page(self):
+        self.make_recipe_sample(6)
+        # STEPS
+        # 1 - Get to the page
+        # 2 - Get to the pagination
+        # 3 - See if there are more than one page
+        self.chrome.get(self.live_server_url)
+        pages = self.chrome.find_elements(By.CLASS_NAME, "pag-item")
+        self.assertEqual(2, len(pages))
