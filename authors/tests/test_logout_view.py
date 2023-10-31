f9737111ed607ca6fe456e70517bc98d9d0d8d67
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-10-30 23:40:33 -0300

    test: adicionando método de apoio aos testes

--- authors/tests/test_logout_view.py
+++ authors/tests/test_logout_view.py
@@ -4,6 +4,13 @@ from django.urls import resolve, reverse
 
 
 class LogoutViewTest(FormTestBase):
+    def login_user(self):
+        self.create_user()
+        self.client.login(
+            username=self.form_data['username'],
+            password=self.form_data['password']
+        )
+
     def test_logout_is_loading_correct_view(self):
         view = resolve(reverse('authors:logout'))
         self.assertIs(view.func, views.logout_view)
@@ -16,14 +23,23 @@ class LogoutViewTest(FormTestBase):
                          response.request.get('PATH_INFO'))
 
     def test_logout_view_is_logging_out_user(self):
-        self.create_user()
-        self.client.post(
-            reverse('authors:login_create'),
-            data={'username': self.form_data['username'],
-                  'password': self.form_data['password']})
-        response = self.client.post(reverse('authors:logout'),
-                                    data={
-            'username': self.form_data['username'],
-        }, follow=True)
+        self.login_user()
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': self.form_data['username'],
+            },
+            follow=True)
         content = response.content.decode('utf-8')
         self.assertIn('You have sucessfully logged out', content)
+
+    def test_logout_view_redirect_to_login_when_data_does_not_match(self):
+        self.login_user()
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': f'{self.form_data["username"]}1',
+            },
+            follow=True)
+        self.assertEqual(reverse('authors:login'),
+                         response.request.get('PATH_INFO'))
