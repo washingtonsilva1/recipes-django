6289c085c484990db30a23843e5e84a4ec7f24b6
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-10-30 23:37:27 -0300

    atualizando arquivos

--- utils/browser.py
+++ utils/browser.py
@@ -1,18 +1,11 @@
 import os
 from selenium import webdriver
-from time import sleep
 
 
 def make_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
-    if os.environ.get('CHROMEDRIVER_OPTIONS', '').strip():
-        for option in os.environ.get('CHROMEDRIVER_OPTIONS').split(','):
+    options = os.environ.get('CHROMEDRIVER_OPTIONS', '')
+    if options:
+        for option in options.strip().split(','):
             chrome_options.add_argument(option.strip())
     return webdriver.Chrome(options=chrome_options)
-
-
-if __name__ == '__main__':
-    browser = make_chrome_driver()
-    browser.get('https://www.google.com.br')
-    sleep(5)
-    browser.quit()
