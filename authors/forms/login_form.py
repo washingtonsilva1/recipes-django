2c83553e8177f9e1a374c60d8f8e616d9288aaab
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-10-30 23:39:10 -0300

    refactor: alterando mensagem para campo requerido

--- authors/forms/login_form.py
+++ authors/forms/login_form.py
@@ -7,7 +7,12 @@ class LoginForm(forms.Form):
         widget=forms.TextInput(
             attrs={
                 'placeholder': 'Type your username',
-            })
+            }
+        ),
+        error_messages={
+            'required': 'Username can not be empty.',
+        }
+
     )
     password = forms.CharField(
         label='Password',
@@ -15,5 +20,8 @@ class LoginForm(forms.Form):
             attrs={
                 'placeholder': 'Type your password'
             }
-        )
+        ),
+        error_messages={
+            'required': 'Password can not be empty.',
+        }
     )
