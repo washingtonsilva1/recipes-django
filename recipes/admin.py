cf34011a590afb727f08b36c3f35de393aad6b64
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-11-02 13:52:11 -0300

    feat: filtros em recipes no painel administrativo

--- recipes/admin.py
+++ recipes/admin.py
@@ -8,7 +8,18 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'title', 'created_at',
+                    'is_published', 'user')
+    list_display_links = ('title',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'preparation_steps', 'slug')
+    list_filter = ('is_published', 'category', 'user',
+                   'preparation_steps_is_html')
+    ordering = ('-id',)
+    list_per_page = 10
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
 
 
 # Register your models here.
