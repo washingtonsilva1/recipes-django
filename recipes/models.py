f07034da983d129a117b43ad6fe4154d8413eb5a
washingtonsilva1 <deivisonwashington@hotmail.com>
2023-11-13 13:30:50 -0300

    refactor: recebendo url da própria receita

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.db import models
 
 
@@ -32,3 +33,6 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('recipes:detail', args=(self.pk,))
