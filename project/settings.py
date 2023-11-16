9761bbb815148f46d59dd5cae1266b1ab99eeb61
Deivison Washington <deivisonwashington@hotmail.com>
2023-11-16 10:20:39 -0300

    refactor: garantindo que o valor seja recebido

--- project/settings.py
+++ project/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
         # flake8: noqa
         'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': os.environ.get('DATABASE_NAME', BASE_DIR / 'db.sqlite3'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', ''),
+        'PORT': os.environ.get('DATABASE_PORT', ''),
     }
 }
 
