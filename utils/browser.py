b591ce2b0643ddf6a7ec2b55b815e347aed6f182
Deivison Washington <deivisonwashington@hotmail.com>
2023-11-22 02:03:08 -0300

    delegando parte do trabalho para parse_str_to_list

--- utils/browser.py
+++ utils/browser.py
@@ -1,11 +1,11 @@
 import os
 from selenium import webdriver
+from utils.utils import parse_str_to_list
 
 
 def make_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
     options = os.environ.get('CHROMEDRIVER_OPTIONS', '')
-    if options:
-        for option in options.strip().split(','):
-            chrome_options.add_argument(option.strip())
+    for option in parse_str_to_list(options, ','):
+        chrome_options.add_argument(option)
     return webdriver.Chrome(options=chrome_options)
