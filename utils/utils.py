1337e19f316cd305c3f89757065c6dfeb4c658e8
Deivison Washington <deivisonwashington@hotmail.com>
2023-11-22 02:01:16 -0300

    função para transformar string em lista

--- utils/utils.py
+++ utils/utils.py
@@ -6,3 +6,11 @@ def generate_random_string(n=5) -> str:
     chars = string.ascii_letters + string.digits
     rand = SystemRandom().choices(population=chars, k=n)
     return ''.join(rand)
+
+
+def parse_str_to_list(string: str, sep=',') -> list:
+    if not string or not isinstance(string, str):
+        return []
+    if not sep or not isinstance(sep, str):
+        sep = ','
+    return [s.strip() for s in string.split(sep) if s.strip()]
