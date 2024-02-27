30b87956811ac7f56210353b1862b4560bc204a9
Deivison Washington <deivisonwashington@hotmail.com>
2024-02-27 00:53:06 -0300

    tornando erros mais genéricos

--- recipes/validators.py
+++ recipes/validators.py
@@ -18,10 +18,7 @@ class RecipeValidator:
         description = self.data.get('description', '')
         if description.lower() == title.lower():
             self.errors['description'].append(
-                self.errorClass(
-                    message='Your description and title can not be the same.',
-                    code='invalid'
-                )
+                'Your description and title can not be the same.',
             )
         if self.errors:
             raise self.errorClass(self.errors)
@@ -38,49 +35,34 @@ class RecipeValidator:
         title = self.data.get('title', '')
         if len(title) < 8:
             self.errors['title'].append(
-                self.errorClass(
-                    message='Your title must have at least 8 characters.',
-                    code='invalid'
-                )
+                'Your title must have at least 8 characters.',
             )
         recipe_from_db = Recipe.objects.filter(
             title__iexact=title,
         ).first()
         if recipe_from_db:
             self.errors['title'].append(
-                self.errorClass(
-                    message='A recipe with this title already exists, ' +
-                    'try another one.',
-                    code='invalid'
-                )
+                'A recipe with this title already exists, ' +
+                'try another one.',
             )
 
     def clean_description(self):
         description = self.data.get('description', '')
         if len(description) < 10:
             self.errors['description'].append(
-                self.errorClass(
-                    message='Your description must have at least 10 characters.',
-                    code='invalid'
-                )
+                'Your description must have at least 10 characters.',
             )
 
     def clean_servings(self):
         servings = self.data.get('servings', '')
         if parse_str_to_int(servings) < 1:
             self.errors['servings'].append(
-                self.errorClass(
-                    message='Type a number bigger than zero.',
-                    code='invalid'
-                )
+                'Type a number bigger than zero.'
             )
 
     def clean_preparation_time(self):
         preparation_time = self.data.get('preparation_time', '')
         if parse_str_to_int(preparation_time) < 1:
             self.errors['preparation_time'].append(
-                self.errorClass(
-                    message='Type a number bigger than zero.',
-                    code='invalid'
-                )
+                'Type a number bigger than zero.'
             )
